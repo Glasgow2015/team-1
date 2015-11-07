b9e28882149ba5f0fcee09c4daf6243766786685
Adam Liddell <bwb12175@uni.strath.ac.uk>
2015-11-07 21:16:46 +0000

    start of Hive class

--- Apiary/app/src/main/java/com/example/adam/apiary/Hive.java
+++ Apiary/app/src/main/java/com/example/adam/apiary/Hive.java
@@ -4,4 +4,14 @@ package com.example.adam.apiary;
  * Created by Adam on 07/11/2015.
  */
 public class Hive {
+
+    private int hiveNo;
+    private String location;
+    private int dateday;
+    private int datemonth;
+    private int dateyear;
+    private HiveType hivetype;
+    private SunExposure sun;
+
+
 }
