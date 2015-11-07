b5e8d5f1eb5709db76b8ad462986e750e7c6242a
Adam Liddell <bwb12175@uni.strath.ac.uk>
2015-11-07 20:05:14 +0000

    Initial Android project commit

---
+++ Apiary/app/src/main/java/com/example/adam/apiary/Apiary.java
@@ -0,0 +1,12 @@
+package com.example.adam.apiary;
+
+import java.util.List;
+
+/**
+ * Created by Adam on 07/11/2015.
+ */
+public class Apiary {
+
+    private List<Hive> hives;
+
+}
---
+++ Apiary/app/src/main/java/com/example/adam/apiary/Hive.java
@@ -0,0 +1,7 @@
+package com.example.adam.apiary;
+
+/**
+ * Created by Adam on 07/11/2015.
+ */
+public class Hive {
+}
