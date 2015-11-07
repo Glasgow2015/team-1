7d0c97b6530b0764bbe2afb00c403f061af8e9f9
Adam Liddell <bwb12175@uni.strath.ac.uk>
2015-11-07 20:11:40 +0000

    Added details to Apiary class

--- Apiary/app/src/main/java/com/example/adam/apiary/Apiary.java
+++ Apiary/app/src/main/java/com/example/adam/apiary/Apiary.java
@@ -7,6 +7,10 @@ import java.util.List;
  */
 public class Apiary {
 
+    private String name;
+    private String location;
+    private int year;
+    private List<Character> months;
     private List<Hive> hives;
 
 }
