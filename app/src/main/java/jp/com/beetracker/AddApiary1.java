495908c2d166a2f712b406164690dc90c6cb644b
Baldeep <baldeepsingh.93@gmail.com>
2015-11-08 02:56:46 +0000

    created add apairty java file

---
+++ app/src/main/java/jp/com/beetracker/AddApiary1.java
@@ -0,0 +1,12 @@
+package jp.com.beetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddApiary1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
